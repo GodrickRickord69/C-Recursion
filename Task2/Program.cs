30a66af040923193246230381fc7888b1c70d4de
Nikita <varashilov125@gmail.com>
2023-07-08 11:58:53 +0300

    Task1 числа в промежутке

---
+++ Task2/Program.cs
@@ -0,0 +1,32 @@
+﻿/*Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30*/
+
+
+Console.WriteLine("Введите число M: ");
+int M = int.Parse(Console.ReadLine()!);
+
+
+Console.WriteLine("Введите число N: ");
+int N = int.Parse(Console.ReadLine()!);
+
+
+
+Console.WriteLine($"{GetSumNumbers(M, N)} ");
+
+
+int GetSumNumbers(int NumbM, int NumbN)
+{
+    if (NumbM == NumbN)
+    {
+
+        return NumbM;
+    }
+    else
+    {
+        return NumbM + GetSumNumbers(NumbM + 1, NumbN);
+    }
+
+    // SumNumbers = SumNumbers + (NumbM++);
+    // GetSumNumbers(NumbM, NumbN, SumNumbers);
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n. Выполнить с помощью рекурсии.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29*/
+
+
+Console.WriteLine("Введите число m: ");
+int m = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите число n: ");
+int n = int.Parse(Console.ReadLine()!);
+
+int AkkermanFunction =;
+
